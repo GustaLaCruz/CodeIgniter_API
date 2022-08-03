352e17dfcb3f5f748e1e12df9b5fb6f9b21e41ba
GustaLaCruz <sprungsumobr@gmail.com>
2022-08-03 14:56:55 -0300

    Update SeedProdutos.php

--- app/Database/Seeds/SeedProdutos.php
+++ app/Database/Seeds/SeedProdutos.php
@@ -8,9 +8,9 @@ class SeedProdutos extends Seeder
 {
     public function run()
     {
-        $descricao =['PC Gamer Kratos','PC Gamer Asgard','PC Gamer Valhalla','PC Gamer Loki','PC Gamer Odin'];
-        $valor =[9000,7800,15000,4500,23000];
-        for($x = 0; $x<5; $x++){
+        $descricao =['PC Gamer Kratos','PC Gamer Asgard','PC Gamer Valhalla','PC Gamer Loki','PC Gamer Odin','PC Gamer Balder'];
+        $valor =[9000,7800,15000,4500,23000,7000];
+        for($x = 0; $x<6; $x++){
             $data = [
                 'descricao' => $descricao[$x],
                 'valor'    => $valor[$x],
